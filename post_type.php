befbf0db7ef9a9a8c699ce59f24390d3b7677e8b
Raymond Tomcat <swordart2050@gmail.com>
2022-05-06 00:59:52 +0430

    Create post_type.php

---
+++ post_type.php
@@ -0,0 +1,18 @@
+// Post type Portfolios
+
+add_action( 'init', 'portfolios' );
+function portfolios() {
+    register_post_type( 'portfolios',
+        array(
+            'labels' => array(
+                'name' => 'Portfolios',
+                'singular_name' => 'Portfolio'
+            ),
+            'public' => true,
+            'rewrite' => array( 'slug' => 'portfolios' ),
+            'menu_icon' => 'dashicons-slides',
+            'supports' => array('title', 'thumbnail', 'editor'),
+            'taxonomies' => array('category'),
+        )
+    );
+}
