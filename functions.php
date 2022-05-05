d0947ddda4638722b7f2a9718bb0db1f998da44c
Raymond Tomcat <swordart2050@gmail.com>
2022-05-06 00:48:22 +0430

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+// Create Custom Links
+
+define("RaymondTomcat_URL", get_template_directory_uri());
+
+// Importing Files
+
+function RaymondTomcat_scripts()
+{
+    wp_enqueue_style('bootstrap-rtl',RaymondTomcat_URL . '/css/bootstrap.rtl.min.css');
+
+    wp_enqueue_script('jquery_js',RaymondTomcat_URL . '/js/jquery.min.js',array('jquery'),true);
+    wp_enqueue_script('bootstrap_js',RaymondTomcat_URL . '/js/bootstrap.min.js',array('jquery'),true);
+    wp_enqueue_script('mixitup_min_js',RaymondTomcat_URL . '/js/mixitup.min.js',array(),true);
+}
+add_action('wp_enqueue_scripts','RaymondTomcat_scripts');
