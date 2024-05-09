f42ff2f647abb0176c86a9d220e27ce8c0124b11
Raymond Baghumian <swordart2050@gmail.com>
2024-05-10 02:55:44 +0330

    Changing Function

--- functions.php
+++ functions.php
@@ -1,19 +1,19 @@
 <?php
 
-// Create Custom Links
+// Create Custom Link
 
-define("RaymondTomcat_URL", get_template_directory_uri());
+define("Rayium_URL", get_template_directory_uri());
 
 // Importing Files
 
-function RaymondTomcat_scripts()
+function Rayium_scripts()
 {
-    wp_enqueue_style('bootstrap_css',RaymondTomcat_URL . '/css/bootstrap.css');
-    wp_enqueue_style('style_css',RaymondTomcat_URL . '/css/style.css');
-    wp_enqueue_script('jquery_js',RaymondTomcat_URL . '/js/jquery.min.js',array('jquery'),true);
-    wp_enqueue_script('bootstrap_js',RaymondTomcat_URL . '/js/bootstrap.min.js',array('jquery'),true);
-    wp_enqueue_script('mixitup_min_js',RaymondTomcat_URL . '/js/mixitup.min.js',array(),true);
+    wp_enqueue_style('bootstrap_css',Rayium_URL . '/css/bootstrap.css');
+    wp_enqueue_style('style_css',Rayium_URL . '/css/style.css');
+    wp_enqueue_script('jquery_js',Rayium_URL . '/js/jquery.min.js',array('jquery'),true);
+    wp_enqueue_script('bootstrap_js',Rayium_URL . '/js/bootstrap.min.js',array('jquery'),true);
+    wp_enqueue_script('mixitup_min_js',Rayium_URL . '/js/mixitup.min.js',array(),true);
 }
-add_action('wp_enqueue_scripts','RaymondTomcat_scripts');
+add_action('wp_enqueue_scripts','Rayium_scripts');
 
 require_once('post_type.php');
